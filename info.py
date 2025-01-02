fadb7629ab10a1549f35e84d97cb4544bb35800e
TmrBotz <tmrbotz@gmail.com>
2025-01-02 15:44:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/TMR_movie_request_group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Tmr_Botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Tmr_Botz_Support) # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Tmr_Botz_Support') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Tmr_Developer')
 
 # True Or False
