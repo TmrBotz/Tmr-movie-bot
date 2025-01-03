fcdee4a60956e73f04c2ec2151a9ad58a88ae41d
TmrBotz <tmrbotz@gmail.com>
2025-01-03 14:35:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -627,7 +627,7 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(button)
             )
             btn = [[
-                InlineKeyboardButton("Get File Again", callback_data=f'https://t.me/TMR_movie_request_group')
+                InlineKeyboardButton("Get File Again", url=GRP_LNK)
             ]]
             k = await msg.reply("<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</i></b>",quote=True)
             await asyncio.sleep(600)
@@ -676,7 +676,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(button)
     )
     btn = [[
-        InlineKeyboardButton("Get File Again", callback_data=f'https://t.me/TMR_movie_request_group')
+        InlineKeyboardButton("Get File Again", url=GRP_LNK)
     ]]
     k = await msg.reply("<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</i></b>",quote=True)
     await asyncio.sleep(600)
